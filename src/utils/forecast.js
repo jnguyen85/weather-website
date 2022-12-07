2d3794643d3184ad836a6ea04d6528ec4ecc0903
John <nguyen8585@gmail.com>
2022-12-07 07:43:57 -0800

    add humidity to weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,21 +3,20 @@ const request = require("request");
 const forecast = (lat, lon, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=e24641645de3aed91656dea18c196303&query=${lat},${lon}&units=f`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
-      console.log("Unable to connect to weather service!");
-    } else if (response.body.error) {
-      console.log("Unable to find location");
+      callback("Unable to connect to weather service!");
+    } else if (body.error) {
+      callback("Unable to find location");
     } else {
-      const currentTemp = response.body.current.temperature;
-      const feelTemp = response.body.current.feelslike;
-      const weatherDescription = response.body.current.weather_descriptions[0];
-      console.log(
-        `${weatherDescription}. It it currently ${currentTemp} out. It feels like ${feelTemp} degrees out.`
-      );
+      const currentTemp = body.current.temperature;
+      const feelTemp = body.current.feelslike;
+      const weatherDescription = body.current.weather_descriptions[0];
+      const humidity = body.current.humidity;
+
       callback(
         undefined,
-        `${weatherDescription}. It it currently ${currentTemp} out. It feels like ${feelTemp} degrees out.`
+        `${weatherDescription}. It it currently ${currentTemp} out. It feels like ${feelTemp} degrees out. The humidity is ${humidity}`
       );
     }
   });
